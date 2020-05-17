c0859689706f60deb0149ad16903dec73ac5547f
jonapoul <jonathanpoulton1@gmail.com>
2020-05-17 10:04:35 +0100

    Aesthetics: Reordered Key class to match settings.xml

--- app/src/main/java/com/jon/cotgenerator/utils/Key.java
+++ app/src/main/java/com/jon/cotgenerator/utils/Key.java
@@ -5,17 +5,17 @@ public final class Key {
 
     public static final String TRANSMITTED_DATA = "transmittedData";
     public static final String CALLSIGN = "callsign";
-    public static final String CENTRE_LATITUDE = "centreLatitude";
-    public static final String CENTRE_LONGITUDE = "centreLongitude";
     public static final String ICON_COUNT = "iconCount";
-    public static final String LOCATION_GROUP = "locationGroup";
-    public static final String MOVEMENT_RADIUS = "movementRadius";
-    public static final String RADIAL_DISTRIBUTION = "radialDistribution";
     public static final String STALE_TIMER = "staleTimer";
     public static final String RANDOM_COLOUR = "randomColour";
     public static final String TEAM_COLOUR = "teamColour";
-    public static final String TRANSMISSION_PERIOD = "transmissionPeriod";
+    public static final String CENTRE_LATITUDE = "centreLatitude";
+    public static final String CENTRE_LONGITUDE = "centreLongitude";
+    public static final String LOCATION_GROUP = "locationGroup";
+    public static final String RADIAL_DISTRIBUTION = "radialDistribution";
+    public static final String MOVEMENT_RADIUS = "movementRadius";
     public static final String TRANSMISSION_PROTOCOL = "transmissionProtocol";
+    public static final String TRANSMISSION_PERIOD = "transmissionPeriod";
     public static final String UDP_GROUP = "udpGroup";
     public static final String UDP_ADDRESS = "udpAddress";
     public static final String UDP_PORT = "udpPort";
