3e09181e92762a00c7cbc3f3ffd5901432abdfe9
jonapoul <jonathanpoulton1@gmail.com>
2020-10-26 18:40:07 +0000

    Auto-generate a callsign for each beacon instance
    This avoids a scenario in which lots of people use it in the same network
    and they all give themselves the default callsign of "BEACON"

--- cotbeacon/src/main/java/com/jon/cotbeacon/BeaconSettingsFragment.kt
+++ cotbeacon/src/main/java/com/jon/cotbeacon/BeaconSettingsFragment.kt
@@ -1,7 +1,23 @@
 package com.jon.cotbeacon
 
+import android.os.Bundle
+import androidx.preference.EditTextPreference
+import com.jon.common.prefs.CommonPrefs
 import com.jon.common.ui.main.SettingsFragment
 
 class BeaconSettingsFragment : SettingsFragment() {
-    /* Implementation differences TBC */
+    override fun onCreatePreferences(savedState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedState, rootKey)
+
+        /* If callsign hasn't previously been configured, pull a random ATAK default callsign and
+         * assign it to this beacon. */
+        if (prefs.getString(CommonPrefs.CALLSIGN.key, null) == null) {
+            val atakDefaultCallsigns = resources.getStringArray(R.array.atakCallsigns)
+            val randomCallsign = atakDefaultCallsigns.random()
+            findPreference<EditTextPreference>(CommonPrefs.CALLSIGN.key)?.text = randomCallsign
+            prefs.edit()
+                    .putString(CommonPrefs.CALLSIGN.key, randomCallsign)
+                    .apply()
+        }
+    }
 }
